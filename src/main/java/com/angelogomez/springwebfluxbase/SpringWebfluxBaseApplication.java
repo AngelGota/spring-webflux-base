b17e53efc9c1e0f8be915d48d1a309ebdf29dc8d
Angelo <gangelos630@gmail.com>
2025-07-01 00:03:14 -0500

    first commit

---
+++ src/main/java/com/angelogomez/springwebfluxbase/SpringWebfluxBaseApplication.java
@@ -0,0 +1,13 @@
+package com.angelogomez.springwebfluxbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxBaseApplication.class, args);
+    }
+
+}
